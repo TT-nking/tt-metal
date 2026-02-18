142e79e44019b84cb5ca992eeafb1850b6bfd0c6
Izajasz Wrosz <iwrosz@tenstorrent.com>
2026-02-18 10:28:46 +0000

    Path for large tensors in layer norm op was not enabled for rms norm.
        This is a refined version of the previously reverted commit 350e309, implemented by bbradelTT.
        Compared to the original, the condition for allocating CB c_24 has been fixed,
        as well as unit tests were added to cover the problematic scenario.
    
        As in the original commit 350e309, this patch:
        - Enables the large tensor path for rms norm
        - Fixes an error: passes rmsnorm define to reader kernel
        - Adds a large tensor rms norm unit test
    
    Co-Author: Borys Bradel <164946524+bbradelTT@users.noreply.github.com>

--- tests/ttnn/unit_tests/operations/fused/test_rms_norm.py
+++ tests/ttnn/unit_tests/operations/fused/test_rms_norm.py
@@ -35,8 +35,12 @@ def test_rms_norm(device, batch_size, h, w):
 
 @pytest.mark.parametrize("batch_size", [1])
 @pytest.mark.parametrize("h", [128])
-@pytest.mark.parametrize("w", [32])
-def test_rms_norm_row_major(device, batch_size, h, w):
+@pytest.mark.parametrize("w", [32, 4096])
+@pytest.mark.parametrize("math_fidelity", [ttnn.MathFidelity.HiFi4, ttnn.MathFidelity.HiFi2])
+@pytest.mark.parametrize("math_approx_mode", [True, False])
+@pytest.mark.parametrize("fp32_dest_acc_en", [True, False])
+@pytest.mark.parametrize("packer_l1_acc", [True, False])
+def test_rms_norm_row_major(device, batch_size, h, w, math_fidelity, math_approx_mode, fp32_dest_acc_en, packer_l1_acc):
     torch.manual_seed(0)
 
     torch_input_tensor = torch.rand((batch_size, h, w), dtype=torch.bfloat16)
@@ -45,8 +49,23 @@ def test_rms_norm_row_major(device, batch_size, h, w):
     torch_output_tensor = golden_function(torch_input_tensor, torch_weight)
 
     input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.TILE_LAYOUT)
-    weight = ttnn.from_torch(torch_weight, device=device, layout=ttnn.ROW_MAJOR_LAYOUT)
-    output_tensor = ttnn.rms_norm(input_tensor, weight=weight)
+
+    # For ROW_MAJOR layout, weight's last padded dim needs to equal tile width,
+    # additionally, weight's volume needs to be equal to the last padded dim of the input.
+    tile_width = 32
+    assert w % tile_width == 0
+    torch_weight_reshaped = torch_weight.reshape(w // tile_width, tile_width)
+    weight = ttnn.from_torch(torch_weight_reshaped, device=device, layout=ttnn.ROW_MAJOR_LAYOUT)
+
+    compute_config = ttnn.init_device_compute_kernel_config(
+        device.arch(),
+        math_fidelity=math_fidelity,
+        math_approx_mode=math_approx_mode,
+        fp32_dest_acc_en=fp32_dest_acc_en,
+        packer_l1_acc=packer_l1_acc,
+    )
+
+    output_tensor = ttnn.rms_norm(input_tensor, weight=weight, compute_kernel_config=compute_config)
     output_tensor = ttnn.from_device(output_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
 
@@ -74,3 +93,49 @@ def test_rms_norm_with_weight_and_residual(device, batch_size, h, w, dtype):
     output_tensor = ttnn.to_torch(output_tensor)
 
     assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
+
+
+@pytest.mark.parametrize("batch_size, w", [(1, 5120)])
+def test_large_tensor_rms_norm(device, batch_size, w):
+    torch.manual_seed(0)
+
+    epsilon = 9.99999997e-7
+
+    torch_input_tensor = torch.rand((batch_size, w), dtype=torch.bfloat16)
+    torch_weight = torch.rand((w,), dtype=torch.bfloat16)
+    golden_function = ttnn.get_golden_function(ttnn.rms_norm)
+    torch_output_tensor = golden_function(torch_input_tensor, torch_weight, epsilon=epsilon)
+
+    # Multiple iterations to test with program cache
+    for _ in range(3):
+        input_tensor = ttnn.from_torch(
+            torch_input_tensor,
+            device=device,
+            layout=ttnn.TILE_LAYOUT,
+            dtype=ttnn.bfloat16,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        )
+        weight = ttnn.from_torch(
+            torch_weight,
+            device=device,
+            layout=ttnn.TILE_LAYOUT,
+            dtype=ttnn.bfloat16,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        )
+
+        compute_config = ttnn.WormholeComputeKernelConfig(
+            math_fidelity=ttnn.MathFidelity.HiFi4,
+            math_approx_mode=False,
+            fp32_dest_acc_en=True,
+            packer_l1_acc=True,
+        )
+
+        output_tensor = ttnn.rms_norm(
+            input_tensor,
+            weight=weight,
+            epsilon=epsilon,
+            compute_kernel_config=compute_config,
+        )
+        output_tensor = ttnn.from_device(output_tensor)
+        output_tensor = ttnn.to_torch(output_tensor)
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.9999)
