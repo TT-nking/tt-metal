dfbbf0e42cbf451210ac25fb797535088ccd0585
Sudhanshu Singhal <ssinghalTT@tenstorrent.com>
2026-02-23 10:01:49 +0000

    removed backward compat

--- models/tt_transformers/tt/generator_vllm.py
+++ models/tt_transformers/tt/generator_vllm.py
@@ -414,18 +414,7 @@ class LlamaForCausalLM(Generator):
         return super().prefill_forward_text(*args, **kwargs)
 
     def decode_forward(self, *args, **kwargs):
-        result = super().decode_forward_text(*args, **kwargs)
-        # Handle backward compatibility: decode_forward_text may return tuples.
-        # When read_from_device=False: returns list of tuples [(tt_logits, tt_log_probs), ...]
-        # When read_from_device=True: returns tuple (logits, log_probs) from process_decode_output_host
-        # Older vLLM versions expect just the logits tensor, not tuples.
-        read_from_device = kwargs.get("read_from_device", True)
-        if not read_from_device and isinstance(result, list) and len(result) > 0:
-            if isinstance(result[0], tuple):
-                result = [item[0] for item in result]
-        elif read_from_device and isinstance(result, tuple):
-            result = result[0]  # Return just the logits tensor
-        return result
+        return super().decode_forward_text(*args, **kwargs)
 
     def allocate_kv_cache(self, *args, **kwargs):
         return allocate_vllm_kv_cache(*args, **kwargs, dp_model=self.model, tt_cache_path=self.cache_path)
@@ -473,18 +462,7 @@ class QwenForCausalLM(Generator):
         return super().prefill_forward_text(*args, **kwargs)
 
     def decode_forward(self, *args, **kwargs):
-        result = super().decode_forward_text(*args, **kwargs)
-        # Handle backward compatibility: decode_forward_text may return tuples.
-        # When read_from_device=False: returns list of tuples [(tt_logits, tt_log_probs), ...]
-        # When read_from_device=True: returns tuple (logits, log_probs) from process_decode_output_host
-        # Older vLLM versions expect just the logits tensor, not tuples.
-        read_from_device = kwargs.get("read_from_device", True)
-        if not read_from_device and isinstance(result, list) and len(result) > 0:
-            if isinstance(result[0], tuple):
-                result = [item[0] for item in result]
-        elif read_from_device and isinstance(result, tuple):
-            result = result[0]  # Return just the logits tensor
-        return result
+        return super().decode_forward_text(*args, **kwargs)
 
     def allocate_kv_cache(self, *args, **kwargs):
         return allocate_vllm_kv_cache(*args, **kwargs, dp_model=self.model, tt_cache_path=self.cache_path)
@@ -532,18 +510,7 @@ class MistralForCausalLM(Generator):
         return super().prefill_forward_text(*args, **kwargs)
 
     def decode_forward(self, *args, **kwargs):
-        result = super().decode_forward_text(*args, **kwargs)
-        # Handle backward compatibility: decode_forward_text may return tuples.
-        # When read_from_device=False: returns list of tuples [(tt_logits, tt_log_probs), ...]
-        # When read_from_device=True: returns tuple (logits, log_probs) from process_decode_output_host
-        # Older vLLM versions expect just the logits tensor, not tuples.
-        read_from_device = kwargs.get("read_from_device", True)
-        if not read_from_device and isinstance(result, list) and len(result) > 0:
-            if isinstance(result[0], tuple):
-                result = [item[0] for item in result]
-        elif read_from_device and isinstance(result, tuple):
-            result = result[0]  # Return just the logits tensor
-        return result
+        return super().decode_forward_text(*args, **kwargs)
 
     def allocate_kv_cache(self, *args, **kwargs):
         return allocate_vllm_kv_cache(*args, **kwargs, dp_model=self.model, tt_cache_path=self.cache_path)
@@ -681,18 +648,7 @@ class Gemma3ForConditionalGeneration(Generator, SupportsMultiModal):
         return allocate_vllm_kv_cache(*args, **kwargs, dp_model=self.model, tt_cache_path=self.cache_path)
 
     def decode_forward(self, *args, **kwargs):
-        result = super().decode_forward_text(*args, **kwargs)
-        # Handle backward compatibility: decode_forward_text may return tuples.
-        # When read_from_device=False: returns list of tuples [(tt_logits, tt_log_probs), ...]
-        # When read_from_device=True: returns tuple (logits, log_probs) from process_decode_output_host
-        # Older vLLM versions expect just the logits tensor, not tuples.
-        read_from_device = kwargs.get("read_from_device", True)
-        if not read_from_device and isinstance(result, list) and len(result) > 0:
-            if isinstance(result[0], tuple):
-                result = [item[0] for item in result]
-        elif read_from_device and isinstance(result, tuple):
-            result = result[0]  # Return just the logits tensor
-        return result
+        return super().decode_forward_text(*args, **kwargs)
 
 
 class GptOssForCausalLM(Generator):
@@ -761,18 +717,7 @@ class GptOssForCausalLM(Generator):
         return super().prefill_forward_text(*args, **kwargs)
 
     def decode_forward(self, *args, **kwargs):
-        result = super().decode_forward_text(*args, **kwargs)
-        # Handle backward compatibility: decode_forward_text may return tuples.
-        # When read_from_device=False: returns list of tuples [(tt_logits, tt_log_probs), ...]
-        # When read_from_device=True: returns tuple (logits, log_probs) from process_decode_output_host
-        # Older vLLM versions expect just the logits tensor, not tuples.
-        read_from_device = kwargs.get("read_from_device", True)
-        if not read_from_device and isinstance(result, list) and len(result) > 0:
-            if isinstance(result[0], tuple):
-                result = [item[0] for item in result]
-        elif read_from_device and isinstance(result, tuple):
-            result = result[0]  # Return just the logits tensor
-        return result
+        return super().decode_forward_text(*args, **kwargs)
 
     def allocate_kv_cache(self, *args, **kwargs):
         return allocate_vllm_kv_cache(*args, **kwargs, dp_model=self.model, tt_cache_path=self.cache_path)
